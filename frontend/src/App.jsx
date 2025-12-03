772e3b9c6b16890dc1d8471c41d1c57e3d361098
Alejandro <brayanalejandroa03@gmail.com>
2025-12-03 16:44:43 -0500

    nuevo cambio02/12

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
 import Home from './pages/Home';
 import Adoptar from './pages/Adoptar';
-import Login from './pages/Login';
 import Perfil from './pages/perfil';
 import Notificaciones from './pages/Notificaciones';
-import Registro from './pages/Registro';
+import Registro from './pages/registro';
 import Publicar from './pages/publicar';
 import Chat from './pages/Chat';
 
@@ -13,14 +13,14 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Registro />} />
         <Route path="/" element={<Home />} />
         <Route path="/adoptar" element={<Adoptar />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/perfil" element={<Perfil />} />
         <Route path="/notificaciones" element={<Notificaciones />} />
-        <Route path="/registro" element={<Registro />} />
         <Route path="/publicar" element={<Publicar />} />
-        <Route path="/mensajes" element={<Chat />} />  {/* ← CAMBIAR AQUÍ */}
+        <Route path="/mensajes" element={<Chat />} />
       </Routes>
     </BrowserRouter>
   );
